6cc5c515dc441dde6fc260a99d3e93c434f5f3c3
finnwilliamsml <finn.williams@baesystems.com>
2025-04-25 11:58:17 +0100

    Update 02_install_cuda.sh

--- scripts/02_install_cuda.sh
+++ scripts/02_install_cuda.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
-git clone https://github.com/google-deepmind/searchless_chess.git
-cd searchless_chess
-git clone https://github.com/official-stockfish/Stockfish.git
+
+wget https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda-repo-debian12-12-8-local_12.8.1-570.124.06-1_amd64.deb
+dpkg -i cuda-repo-debian12-12-8-local_12.8.1-570.124.06-1_amd64.deb
+cp /var/cuda-repo-debian12-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+apt-get update
+apt-get -y install cuda-toolkit-12-8
