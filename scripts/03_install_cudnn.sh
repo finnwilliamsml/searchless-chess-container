bd1ca72ea3be30bd01f8f1067e0ffae557dc8a37
finnwilliamsml <finn.williams@baesystems.com>
2025-04-25 11:55:03 +0100

    Update 03_install_cudnn.sh

--- scripts/03_install_cudnn.sh
+++ scripts/03_install_cudnn.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+set -e
+cd /app/searchless_chess
+pip install --no-cache-dir -r requirements.txt
+pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
