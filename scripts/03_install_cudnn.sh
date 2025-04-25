73140523b6bd863384aff633859bcafc27c6456a
finnwilliamsml <finn.williams@baesystems.com>
2025-04-25 11:58:30 +0100

    Update 03_install_cudnn.sh

--- scripts/03_install_cudnn.sh
+++ scripts/03_install_cudnn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
-cd /app/searchless_chess
-pip install --no-cache-dir -r requirements.txt
-pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+wget https://developer.download.nvidia.com/compute/cudnn/9.8.0/local_installers/cudnn-local-repo-debian12-9.8.0_1.0-1_amd64.deb
+dpkg -i cudnn-local-repo-debian12-9.8.0_1.0-1_amd64.deb
+cp /var/cuda-repo-debian12-9-8-local/cudnn-*-keyring.gpg /usr/share/keyrings/
+apt-get update
+apt-get -y install cudnn
