e20d1c0fe9627b17e7302b6fe6abe02e0bfa27fc
finnwilliamsml <finn.williams@baesystems.com>
2025-04-25 11:59:20 +0100

    Update 07_setup_bayeselo.sh

--- scripts/07_setup_bayeselo.sh
+++ scripts/07_setup_bayeselo.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 set -e
-# Download Lc0 network
-mkdir -p /app/lc0/build/release
-cd /app/lc0/build/release
-wget https://storage.lczero.org/files/768x15x24h-t82-swa-7464000.pb.gz
-gzip -d 768x15x24h-t82-swa-7464000.pb.gz
 
-# Download and build BayesElo
 cd /app
 wget https://www.remi-coulom.fr/Bayesian-Elo/bayeselo.tar.bz2
 tar -xvjf bayeselo.tar.bz2
 cd BayesElo
 make bayeselo
-
-# Run data download scripts
-cd /app/data
-chmod +x download.sh && ./download.sh
-cd /app/checkpoints
-chmod +x download.sh && ./download.sh
-
