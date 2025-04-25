132bebe27262e68730e5d24188d43481e1de3ae7
finnwilliamsml <finn.williams@baesystems.com>
2025-04-25 11:58:55 +0100

    Update 05_build_stockfish.sh

--- scripts/05_build_stockfish.sh
+++ scripts/05_build_stockfish.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-cd /app
-wget https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda-repo-debian12-12-8-local_12.8.1-570.124.06-1_amd64.deb
-dpkg -i cuda-repo-debian12-12-8-local_12.8.1-570.124.06-1_amd64.deb
-cp /var/cuda-repo-debian12-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
-apt-get update
-apt-get -y install cuda-toolkit-12-8
 
+git clone https://github.com/official-stockfish/Stockfish.git
+cd Stockfish/src
+make -j profile-build ARCH=x86-64-avx2
+cd ../..
