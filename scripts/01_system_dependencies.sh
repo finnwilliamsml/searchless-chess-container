f70115a317ab8ba93bb83bfc9980579d716a7ff3
finnwilliamsml <finn.williams@baesystems.com>
2025-04-25 11:49:02 +0100

    Update 01_system_dependencies.sh

--- scripts/01_system_dependencies.sh
+++ scripts/01_system_dependencies.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+set -e
+
+apt-get update && apt-get install -y \
+    build-essential \
+    git \
+    wget \
+    curl \
+    ninja-build \
+    meson \
+    libgtest-dev \
+&& rm -rf /var/lib/apt/lists/*
 
