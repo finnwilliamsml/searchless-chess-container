443b4331c3ef674f6b57bcb221b3f6bd57e3b891
finnwilliamsml <finn.williams@baesystems.com>
2025-04-25 11:55:37 +0100

    Update 06_download_lc0_weights.sh

--- scripts/06_download_lc0_weights.sh
+++ scripts/06_download_lc0_weights.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+set -e
+cd /app
+wget https://developer.download.nvidia.com/compute/cudnn/9.8.0/local_installers/cudnn-local-repo-debian12-9.8.0_1.0-1_amd64.deb
+dpkg -i cudnn-local-repo-debian12-9.8.0_1.0-1_amd64.deb
+cp /var/cuda-repo-debian12-9-8-local/cudnn-*-keyring.gpg /usr/share/keyrings/
+apt-get update
+apt-get -y install cudnn
 
