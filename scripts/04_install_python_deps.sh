cfde4ad7b12441ad94cacc1acb7b83bb5e5f018c
finnwilliamsml <finn.williams@baesystems.com>
2025-05-01 16:55:21 +0100

    Update 04_install_python_deps.sh

--- scripts/04_install_python_deps.sh
+++ scripts/04_install_python_deps.sh
@@ -5,7 +5,6 @@ git clone https://github.com/google-deepmind/searchless_chess.git
 cd searchless_chess
 
 pip install --no-cache-dir -r requirements.txt
-pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 cd ..
 
