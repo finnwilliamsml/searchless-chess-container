eda23a1b095455156cbb3e0089bdbcdf1e699d64
finnwilliamsml <finn.williams@baesystems.com>
2025-05-02 12:20:44 +0100

    Update 04_install_python_deps.sh

--- scripts/04_install_python_deps.sh
+++ scripts/04_install_python_deps.sh
@@ -15,4 +15,5 @@ pip install --no-cache-dir pandas
 pip install --no-cache-dir scipy
 pip install --no-cache-dir typing-extensions
 
-
+wget https://files.pythonhosted.org/packages/31/25/32c5e2c919da4faaea9ef5088437ab6e01738c49402e4ec8a6c7b49e30ef/jax-0.6.0-py3-none-any.whl
+wget https://files.pythonhosted.org/packages/7c/16/30adb773e3db553ac9a8fa6688e3f65f7ccc39798088ba4cf77e9d9314c3/jaxlib-0.6.0-cp310-cp310-manylinux2014_x86_64.whl
