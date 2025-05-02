ab0f9a51e2b09a1a81de81979e1f562b60befbe9
finnwilliamsml <finn.williams@baesystems.com>
2025-05-02 09:44:45 +0100

    Update 04_install_python_deps.sh

--- scripts/04_install_python_deps.sh
+++ scripts/04_install_python_deps.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 
-pip install --no-cache-dir -r absl-py
-pip install --no-cache-dir -r apache-beam
-pip install --no-cache-dir -r chess
-pip install --no-cache-dir -r chex
-pip install --no-cache-dir -r dm-haiku
-pip install --no-cache-dir -r grain-nightly
-pip install --no-cache-dir -r jupyter
-pip install --no-cache-dir -r numpy
-pip install --no-cache-dir -r optax
-pip install --no-cache-dir -r orbax-checkpoint
-pip install --no-cache-dir -r pandas
-pip install --no-cache-dir -r scipy
-pip install --no-cache-dir -r typing-extensions
+pip install --no-cache-dir absl-py
+pip install --no-cache-dir apache-beam
+pip install --no-cache-dir chess
+pip install --no-cache-dir chex
+pip install --no-cache-dir dm-haiku
+pip install --no-cache-dir grain-nightly
+pip install --no-cache-dir jupyter
+pip install --no-cache-dir numpy
+pip install --no-cache-dir optax
+pip install --no-cache-dir orbax-checkpoint
+pip install --no-cache-dir pandas
+pip install --no-cache-dir scipy
+pip install --no-cache-dir typing-extensions
 
 pip download jax==0.6.0 jaxlib==0.6.0+cuda12.cudnn89 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
