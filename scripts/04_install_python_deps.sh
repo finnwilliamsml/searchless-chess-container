a2fb097467b4dcf617779d361306d28018c00490
finnwilliamsml <finn.williams@baesystems.com>
2025-05-02 11:46:08 +0100

    Update 04_install_python_deps.sh

--- scripts/04_install_python_deps.sh
+++ scripts/04_install_python_deps.sh
@@ -14,6 +14,7 @@ pip install --no-cache-dir orbax-checkpoint
 pip install --no-cache-dir pandas
 pip install --no-cache-dir scipy
 pip install --no-cache-dir typing-extensions
+pip install --no-cache-dir --upgrade "jax[cuda12]"
 
 pip download jax==0.6.0 jaxlib==0.6.0+cuda12.cudnn89 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
