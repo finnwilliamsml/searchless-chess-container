f169b7ca1f1ec630d5c8933932a0893b9a178828
finnwilliamsml <finn.williams@baesystems.com>
2025-05-02 09:33:41 +0100

    Update 04_install_python_deps.sh

--- scripts/04_install_python_deps.sh
+++ scripts/04_install_python_deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-pip install --no-cache-dir -r /scripts/requirements.txt
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pip install --no-cache-dir -r "$SCRIPT_DIR/requirements.txt"
 
 pip download jax==0.6.0 jaxlib==0.6.0+cuda12.cudnn89 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
